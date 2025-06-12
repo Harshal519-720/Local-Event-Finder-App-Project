4c3510ec25dae7df7ddf7438f97506c70f84414d
P3acehavenLi0n <60113639+P3acehavenLi0n@users.noreply.github.com>
2025-06-12 12:30:27 -0400

    Update Local Event Finder.py

--- Local Event Finder.py
+++ Local Event Finder.py
@@ -23,7 +23,7 @@ def get_eventbrite_events(city_name):
 
     url = "https://www.eventbriteapi.com/v3/events/search/"
     headers = {
-        "Authorization": "Bearer YOUR_PERSONAL_OAUTH_TOKEN"
+        "Authorization": "Bearer 7R2GY6M4GBHLWWUPYFZK"
     }
     params = {
         "location.address": city_name,
